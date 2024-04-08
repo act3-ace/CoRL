5d972acb9c1a434b276c40732c7bb1ac5293359c
Benjamin.Heiner <benjamin.heiner@us.af.mil>
2024-04-08 11:35:20 -0400

    fix: updates for the pulling of latest corl to github

--- copy-for-aprs.sh
+++ copy-for-aprs.sh
@@ -5,8 +5,9 @@
 # across to the public repositories.
 #
 
-# COPY START - Move internal directories
-
+#
+# Move internal directories to current to replace with latest versions
+#
 internal_corl_path=$1
 strings=(
     corl/
@@ -27,21 +28,28 @@ for i in "${strings[@]}"; do
     cp -r $internal_corl_path/$i .
 done
 
-# Remove internal specific items
+#
+# Remove internal specific items which provide no value to the 
+# github instance
+#
 rm -rf docker/*.crt
 rm -rf docker/*.sh
 
-# Clean up the files!!!
+#
+# Cleanup all files to remove internal items such as repository 
+# addresses and etc. 
+#
 python $1/sanitize.py
 
+#
 # Install the items need and make sure lock is good
+#
 poetry lock
 poetry install --sync
 
-# verify test pass
-pytest test
-
-# Run the workflow tests for github
+#
+# Run the workflow for github (includes pytest and etc)
+#
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
 
 ./bin/act
@@ -50,5 +58,8 @@ rm -rf bin
 
 # Generate corl pdf
 pip install code_to_pdf
-sudo apt install wkhtmltopdf
+sudo apt update && sudo apt install wget
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+sudo apt install -f ./wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 code_to_pdf --title CoRL-$(date +%F).pdf corl
+rm ./wkhtmltox_0.12.6.1-2.jammy_amd64.deb
